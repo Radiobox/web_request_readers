49f83dbd9938a5d9bb96cc92f9c9c1446d47661f
Brian Tomlinson <darthlukan@gmail.com>
2014-02-04 20:13:51 +0100

    Update and rename receiver.go to puller.go
    Give it an easier to remember and explicit name.  Get and Set are too generic, but nobody can mistake "Pull" or "Push", "grab" or "drop"

--- puller.go
+++ puller.go
@@ -1,13 +1,13 @@
-package model_helpers
+package grabber
 
 // A Receiver is a type that performs its own logic related to
 // receiving data from a request.  This is useful for password types
 // (to automatically hash the value) or anything that needs to perform
 // some type of validation.
-type Receiver interface {
+type Puller interface {
 	// Receive takes a value (as would be sent in a request) and
 	// attempts to read set the Receiver's value to match the passed
 	// in value.  It returns an error if the value isn't valid or
 	// can't be parsed to the Receiver.
-	Receive(interface{}) error
+	Pull(interface{}) error
 }
