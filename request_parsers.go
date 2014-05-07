7fef03784b714aeb6d2d39b85e36ebdf7976f358
Samuel Nelson <valczir.darkvein@gmail.com>
2014-05-07 10:39:09 -0600

    Support parsing a body that is an array with ParseBody

--- request_parsers.go
+++ request_parsers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/objx"
 	"io/ioutil"
 	"strconv"
+	"errors"
 )
 
 var multipartMem int64 = 2 << 20 * 10
@@ -19,14 +20,56 @@ func SetMultipartMem(mem int64) {
 	multipartMem = mem
 }
 
+// ConvertMSIToObjxMap recursively converts map[string]interface{}
+// values to objx.Map.  This is designed around the return types of
+// json.Unmarshal, so it may not work for non-json data.
+func ConvertMSIToObjxMap(value interface{}) interface{} {
+	switch src := value.(type) {
+	case map[string]interface{}:
+		for key, val := range src {
+			src[key] = ConvertMSIToObjxMap(val)
+		}
+		return objx.Map(src)
+	case []interface{}:
+		for index, val := range src {
+			src[index] = ConvertMSIToObjxMap(val)
+		}
+		return src
+	}
+	return value
+}
+
+// ParseParams will parse parameters out of a request body.  The
+// result will be an objx.Map of values, or an error if something
+// unexpected happened.  All map[string]interface{} values are
+// converted to objx.Map before returning.
 func ParseParams(ctx context.Context) (objx.Map, error) {
+	val, err := ParseBody(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if val == nil {
+		return nil, nil
+	}
+	params, ok := val.(objx.Map)
+	if !ok {
+		return nil, errors.New("Cannot use non-map body as params")
+	}
+	return params, nil
+}
+
+// ParseBody will parse a request body, regardless of type.  The body
+// could be a json array, and this will return it properly.  All
+// map[string]interface{} values are converted to objx.Map before
+// returning.
+func ParseBody(ctx context.Context) (interface{}, error) {
 	if params, ok := ctx.Data()["params"]; ok {
 		// We've already parsed this request, so return the cached
 		// parameters.
-		return params.(objx.Map), nil
+		return params, nil
 	}
 	request := ctx.HttpRequest()
-	response := make(objx.Map)
+	var response interface{}
 	contentType, _ := codec_services.ParseContentType(request.Header.Get("Content-Type"))
 	var mimeType string
 	if contentType != nil {
@@ -48,14 +91,15 @@ func ParseParams(ctx context.Context) (objx.Map, error) {
 	case "application/x-www-form-urlencoded":
 		fallthrough
 	case "multipart/form-data":
+		params := make(objx.Map)
 		request.ParseMultipartForm(MultipartMem())
 		if request.MultipartForm != nil {
-			response.Set("files", request.MultipartForm.File)
+			params.Set("files", request.MultipartForm.File)
 			for key, values := range request.MultipartForm.Value {
 				if len(values) == 1 {
-					response.Set(key, values[0])
+					params.Set(key, values[0])
 				} else {
-					response.Set(key, values)
+					params.Set(key, values)
 				}
 			}
 		}
@@ -68,12 +112,14 @@ func ParseParams(ctx context.Context) (objx.Map, error) {
 				// param parser to handle that case, so instead of
 				// always adding a slice of values, I'm only adding
 				// the single value if the length of the slice is 1.
-				response.Set(index, values[0])
+				params.Set(index, values[0])
 			} else {
-				response.Set(index, values)
+				params.Set(index, values)
 			}
 		}
+		response = params
 	}
+	response = ConvertMSIToObjxMap(response)
 	ctx.Data().Set("params", response)
 	return response, nil
 }
